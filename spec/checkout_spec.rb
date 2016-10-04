68d5a4296310a925bf414ca5b80be67c959b1f26
reemplazable <>
2016-10-05 01:29:48 +0200

    final specs reached

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -31,7 +31,7 @@ describe Checkout, '#total' do
 
   promotion_rules = { promotions: ['VOUCHER'] }
   pricing_rules_with_promotions = pricing_rules.merge(promotion_rules)
-  context "with discounted prices #{promotion_rules}" do
+  context "with promoted products #{promotion_rules}" do
     it 'calculates Voucher prices with promotion of buying 3 and paying 2' do
       checkout = Checkout.new (pricing_rules_with_promotions)
       checkout.scan('VOUCHER')
@@ -45,13 +45,58 @@ describe Checkout, '#total' do
   discount_rules = { discounts: ['TSHIRT'] }
   pricing_rules_with_discounts = pricing_rules.merge(discount_rules)
   context "with discounted prices #{discount_rules}" do
-    it 'calculates Voucher prices with promotion of buying 3 and paying 2' do
+    it 'calculates Tshirt prices with discount buying 3 or more for 19.00 each' do
       checkout = Checkout.new (pricing_rules_with_discounts)
       checkout.scan('TSHIRT')
       checkout.scan('TSHIRT')
       checkout.scan('TSHIRT')
       expect(checkout.total).to eq 57.00
       expect(checkout.list).to eq 'T-Shirt, T-Shirt, T-Shirt'
+    end
+  end
+
+  pricing_rules_with_promotions_with_discounts = pricing_rules.merge(discount_rules).merge(promotion_rules)
+  context 'with discounted prices and promoted products' do
+    it 'calculates a bundle price' do
+      checkout = Checkout.new (pricing_rules_with_promotions_with_discounts)
+      checkout.scan('VOUCHER')
+      checkout.scan('TSHIRT')
+      checkout.scan('MUG')
+      expect(checkout.total).to eq 32.50
+      expect(checkout.list).to eq 'Voucher, T-Shirt, Coffee Mug'
+    end
+
+    it 'calculates two vouchers and one tshirt price' do
+      checkout = Checkout.new (pricing_rules_with_promotions_with_discounts)
+      checkout.scan('VOUCHER')
+      checkout.scan('TSHIRT')
+      checkout.scan('VOUCHER')
+      expect(checkout.total).to eq 30.00
+      expect(checkout.list).to eq 'Voucher, T-Shirt, Voucher'
+    end
+
+    it 'calculates applies tshirt discount for four tshirts and one voucher price' do
+      checkout = Checkout.new (pricing_rules_with_promotions_with_discounts)
+      checkout.scan('TSHIRT')
+      checkout.scan('TSHIRT')
+      checkout.scan('TSHIRT')
+      checkout.scan('VOUCHER')
+      checkout.scan('TSHIRT')
+      expect(checkout.total).to eq 81.00
+      expect(checkout.list).to eq 'T-Shirt, T-Shirt, T-Shirt, Voucher, T-Shirt'
+    end
+
+    it 'calculates applies tshirt discount for three tshirts, voucher promotion and the mug price' do
+      checkout = Checkout.new (pricing_rules_with_promotions_with_discounts)
+      checkout.scan('VOUCHER')
+      checkout.scan('TSHIRT')
+      checkout.scan('VOUCHER')
+      checkout.scan('VOUCHER')
+      checkout.scan('MUG')
+      checkout.scan('TSHIRT')
+      checkout.scan('TSHIRT')
+      expect(checkout.total).to eq 74.50
+      expect(checkout.list).to eq 'Voucher, T-Shirt, Voucher, Voucher, Coffee Mug, T-Shirt, T-Shirt'
     end
   end
 end
